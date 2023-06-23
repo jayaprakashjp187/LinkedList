64f336e12390b347b1b3902c57ac48ab4cd0ed32
jayaprakash <jayaprakashjp187@gmail.com>
2023-06-23 15:29:08 +0530

    [creating a simple linked list]

---
+++ DataStructuresLinkedList/LinkedList.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructuresLinkedList
+{
+    internal class LinkedList
+    {
+        private Node head;
+        public void Insert(int data)
+        {
+            Node newNode = new Node(data);
+            if (head == null)
+            {
+                head = newNode;
+            }
+            else
+            {
+                Node current = head;
+                while (current.Next != null)
+                {
+                    current = current.Next;
+                }
+                current.Next = newNode;
+            }
+
+
+        }
+        public void Display()
+        {
+            Node current = head;
+            while (current != null)
+            {
+                Console.WriteLine(current.Data);
+                current = current.Next;
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
---
+++ DataStructuresLinkedList/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace DataStructuresLinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("choose what program u want");
+            Console.WriteLine("1.creating a simple linked list");
+            int choose =Convert.ToInt32(Console.ReadLine());
+            switch(choose)
+            {
+                case 1:
+                    Console.WriteLine("welcome to the creating of simple linked list");
+                    LinkedList l = new LinkedList();
+                    l.Insert(56);
+                    l.Insert(30);
+                    l.Insert(70);
+                    l.Display();
+                    break;
+                    break;
+            }
+        }
+    }
+}
